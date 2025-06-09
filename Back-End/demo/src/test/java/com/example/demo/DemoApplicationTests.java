7beb5b70d0a985300b243c886dc00c4fa471654e
Vladosichek <90644299+Vladosichek@users.noreply.github.com>
2025-06-09 09:15:43 +0200

    Update DemoApplicationTests.java

--- Back-End/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Back-End/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+  "spring.datasource.url=jdbc:h2:mem:testdb",
+  "spring.datasource.driverClassName=org.h2.Driver",
+  "spring.datasource.username=sa",
+  "spring.datasource.password=password",
+  "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class DemoApplicationTests {
 
 	@Test
@@ -11,3 +18,4 @@ class DemoApplicationTests {
 	}
 
 }
+
