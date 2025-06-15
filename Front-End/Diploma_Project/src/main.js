e306533ea113f2eb3c530066d87e6b7371c36698
Vladosichek <90644299+Vladosichek@users.noreply.github.com>
2025-06-15 09:57:54 +0200

    Update main.js

--- Front-End/Diploma_Project/src/main.js
+++ Front-End/Diploma_Project/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+
 createApp(App).use(router).mount('#app')
-axios.defaults.baseURL = 'http://localhost:8080'
